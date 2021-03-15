304476c3b16cdc386ea013e54ea738225837da04
Robson Silva <robson.silva@tripletech.com.br>
2021-03-15 08:15:12 -0300

    controllers

---
+++ Api/Controllers/CustoFixoContoller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Financas.Api.Controllers
+{
+    public class CustoFixoContoller
+    {
+
+    }
+}
