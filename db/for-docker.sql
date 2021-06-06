52fc12a53242ff57153e6dd8e09452f6a2151246
Robson Silva <rms2307@outlook.com>
2021-06-06 18:06:50 -0300

    Updated: estrutura do db

--- db/for-docker.sql
+++ db/for-docker.sql
@@ -28,23 +28,12 @@ CREATE TABLE IF NOT EXISTS `CartaoCredito` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
--- Copiando dados para a tabela financas.CartaoCredito: ~2 rows (aproximadamente)
-/*!40000 ALTER TABLE `CartaoCredito` DISABLE KEYS */;
-INSERT INTO `CartaoCredito` (`Id`, `UserId`, `Bandeira`, `DiaFechamentoFatura`, `DiaVencimentoFatura`, `Limite`) VALUES
-	(1, 1, 1, 10, 20, 10000.00),
-	(2, 1, 0, 0, 0, 0.00);
-/*!40000 ALTER TABLE `CartaoCredito` ENABLE KEYS */;
-
 -- Copiando estrutura para tabela financas.CartaoCreditoBandeira
 CREATE TABLE IF NOT EXISTS `CartaoCreditoBandeira` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `Bandeira` varchar(50) NOT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Copiando dados para a tabela financas.CartaoCreditoBandeira: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `CartaoCreditoBandeira` DISABLE KEYS */;
-/*!40000 ALTER TABLE `CartaoCreditoBandeira` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela financas.CartaoCreditoCompra
 CREATE TABLE IF NOT EXISTS `CartaoCreditoCompra` (
@@ -57,12 +46,6 @@ CREATE TABLE IF NOT EXISTS `CartaoCreditoCompra` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
--- Copiando dados para a tabela financas.CartaoCreditoCompra: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `CartaoCreditoCompra` DISABLE KEYS */;
-INSERT INTO `CartaoCreditoCompra` (`Id`, `Desc`, `Valor`, `DataCompra`, `QtdDeParcelas`, `CartaoCreditoId`) VALUES
-	(1, 'Monitor', 400.00, '2021-06-02', 2, 1);
-/*!40000 ALTER TABLE `CartaoCreditoCompra` ENABLE KEYS */;
-
 -- Copiando estrutura para tabela financas.CartaoCreditoParcela
 CREATE TABLE IF NOT EXISTS `CartaoCreditoParcela` (
   `Id` int NOT NULL AUTO_INCREMENT,
@@ -72,13 +55,6 @@ CREATE TABLE IF NOT EXISTS `CartaoCreditoParcela` (
   `NumeroDaParcela` int NOT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
-
--- Copiando dados para a tabela financas.CartaoCreditoParcela: ~2 rows (aproximadamente)
-/*!40000 ALTER TABLE `CartaoCreditoParcela` DISABLE KEYS */;
-INSERT INTO `CartaoCreditoParcela` (`Id`, `CartaoCreditoCompraId`, `VencimentoParcela`, `ValorParcela`, `NumeroDaParcela`) VALUES
-	(1, 1, '2021-06-20', 200.00, 1),
-	(2, 1, '2021-07-20', 200.00, 2);
-/*!40000 ALTER TABLE `CartaoCreditoParcela` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela financas.CustoDiverso
 CREATE TABLE IF NOT EXISTS `CustoDiverso` (
@@ -91,12 +67,6 @@ CREATE TABLE IF NOT EXISTS `CustoDiverso` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
--- Copiando dados para a tabela financas.CustoDiverso: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `CustoDiverso` DISABLE KEYS */;
-INSERT INTO `CustoDiverso` (`Id`, `UserId`, `Desc`, `Valor`, `Pago`, `Data`) VALUES
-	(1, 1, 'Mesa PC', 120.00, b'0', '2021-06-03');
-/*!40000 ALTER TABLE `CustoDiverso` ENABLE KEYS */;
-
 -- Copiando estrutura para tabela financas.CustoFixo
 CREATE TABLE IF NOT EXISTS `CustoFixo` (
   `Id` int NOT NULL AUTO_INCREMENT,
@@ -107,10 +77,6 @@ CREATE TABLE IF NOT EXISTS `CustoFixo` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Copiando dados para a tabela financas.CustoFixo: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `CustoFixo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `CustoFixo` ENABLE KEYS */;
-
 -- Copiando estrutura para tabela financas.CustoFixoDescricao
 CREATE TABLE IF NOT EXISTS `CustoFixoDescricao` (
   `Id` int NOT NULL AUTO_INCREMENT,
@@ -118,10 +84,6 @@ CREATE TABLE IF NOT EXISTS `CustoFixoDescricao` (
   `Desc` varchar(150) NOT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Copiando dados para a tabela financas.CustoFixoDescricao: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `CustoFixoDescricao` DISABLE KEYS */;
-/*!40000 ALTER TABLE `CustoFixoDescricao` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela financas.Receita
 CREATE TABLE IF NOT EXISTS `Receita` (
@@ -133,20 +95,12 @@ CREATE TABLE IF NOT EXISTS `Receita` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Copiando dados para a tabela financas.Receita: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `Receita` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Receita` ENABLE KEYS */;
-
 -- Copiando estrutura para tabela financas.TipoDeReceita
 CREATE TABLE IF NOT EXISTS `TipoDeReceita` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `Tipo` varchar(50) NOT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Copiando dados para a tabela financas.TipoDeReceita: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `TipoDeReceita` DISABLE KEYS */;
-/*!40000 ALTER TABLE `TipoDeReceita` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela financas.Users
 CREATE TABLE IF NOT EXISTS `Users` (
@@ -159,12 +113,6 @@ CREATE TABLE IF NOT EXISTS `Users` (
   PRIMARY KEY (`Id`),
   UNIQUE KEY `UserName` (`UserName`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
-
--- Copiando dados para a tabela financas.Users: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
-INSERT INTO `Users` (`Id`, `UserName`, `Password`, `NomeCompleto`, `RefreshToken`, `RefreshTokenExpiryTime`) VALUES
-	(1, 'string', '47-32-87-F8-29-8D-BA-71-63-A8-97-90-89-58-F7-C0-EA-E7-33-E2-5D-2E-02-79-92-EA-2E-DC-9B-ED-2F-A8', 'string', 'yNc28S3i9xFg2v1uKPb0XsiqNN8CU9atE0lvI1qU/kA=', '2021-06-11');
-/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
