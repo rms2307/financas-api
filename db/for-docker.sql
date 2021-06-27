91fb13aa15f7934019be39197421e9735421f95d
Robson Silva <rms2307@outlook.com>
2021-06-27 01:02:33 -0300

    Updated: ajustes para publicar no heroku

--- db/for-docker.sql
+++ db/for-docker.sql
@@ -12,12 +12,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
--- Copiando estrutura do banco de dados para financas
-CREATE DATABASE IF NOT EXISTS `financas`;
-USE `financas`;
-
--- Copiando estrutura para tabela financas.CartaoCredito
+-- Copiando estrutura para tabela financas.cartaocredito
 CREATE TABLE IF NOT EXISTS `CartaoCredito` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `UserId` bigint NOT NULL,
@@ -28,14 +23,18 @@ CREATE TABLE IF NOT EXISTS `CartaoCredito` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
--- Copiando estrutura para tabela financas.CartaoCreditoBandeira
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela financas.cartaocreditobandeira
 CREATE TABLE IF NOT EXISTS `CartaoCreditoBandeira` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `Bandeira` varchar(50) NOT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Copiando estrutura para tabela financas.CartaoCreditoCompra
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela financas.cartaocreditocompra
 CREATE TABLE IF NOT EXISTS `CartaoCreditoCompra` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `Desc` varchar(150) NOT NULL,
@@ -46,7 +45,9 @@ CREATE TABLE IF NOT EXISTS `CartaoCreditoCompra` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
--- Copiando estrutura para tabela financas.CartaoCreditoParcela
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela financas.cartaocreditoparcela
 CREATE TABLE IF NOT EXISTS `CartaoCreditoParcela` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `CartaoCreditoCompraId` int NOT NULL,
@@ -56,7 +57,9 @@ CREATE TABLE IF NOT EXISTS `CartaoCreditoParcela` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
--- Copiando estrutura para tabela financas.CustoDiverso
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela financas.custodiverso
 CREATE TABLE IF NOT EXISTS `CustoDiverso` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `UserId` bigint NOT NULL,
@@ -65,9 +68,11 @@ CREATE TABLE IF NOT EXISTS `CustoDiverso` (
   `Pago` bit(1) NOT NULL,
   `Data` date NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
--- Copiando estrutura para tabela financas.CustoFixo
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela financas.custofixo
 CREATE TABLE IF NOT EXISTS `CustoFixo` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `CustoFixoDescricaoId` int NOT NULL,
@@ -75,17 +80,21 @@ CREATE TABLE IF NOT EXISTS `CustoFixo` (
   `Pago` bit(1) NOT NULL,
   `Data` date NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 
--- Copiando estrutura para tabela financas.CustoFixoDescricao
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela financas.custofixodescricao
 CREATE TABLE IF NOT EXISTS `CustoFixoDescricao` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `UserId` bigint NOT NULL,
   `Desc` varchar(150) NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
--- Copiando estrutura para tabela financas.Receita
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela financas.receita
 CREATE TABLE IF NOT EXISTS `Receita` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `UserId` bigint NOT NULL,
@@ -95,24 +104,32 @@ CREATE TABLE IF NOT EXISTS `Receita` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Copiando estrutura para tabela financas.TipoDeReceita
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela financas.tipodereceita
 CREATE TABLE IF NOT EXISTS `TipoDeReceita` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `Tipo` varchar(50) NOT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Copiando estrutura para tabela financas.Users
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela financas.users
 CREATE TABLE IF NOT EXISTS `Users` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `UserName` varchar(50) NOT NULL,
+  `Email` varchar(50) DEFAULT NULL,
   `Password` varchar(130) NOT NULL,
   `NomeCompleto` varchar(150) NOT NULL,
   `RefreshToken` varchar(500) DEFAULT NULL,
   `RefreshTokenExpiryTime` date DEFAULT NULL,
   PRIMARY KEY (`Id`),
-  UNIQUE KEY `UserName` (`UserName`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+  UNIQUE KEY `UserName` (`UserName`),
+  UNIQUE KEY `Email` (`Email`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+
+-- Exportação de dados foi desmarcado.
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
